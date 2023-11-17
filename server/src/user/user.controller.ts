1cb680c4ee82788aa7e68ae56bca8ba978ca34b6
Tarhun <TarhunchicKKK@yandex.by>
2023-11-18 00:24:56 +0300

    Make registration

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,19 +3,18 @@ import {
     Get,
     Post,
     Body,
-    Patch,
-    Param,
-    Delete,
+    UsePipes,
+    ValidationPipe,
 } from '@nestjs/common'
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Post()
+    @UsePipes(new ValidationPipe())
     create(@Body() createUserDto: CreateUserDto) {
         return this.userService.create(createUserDto)
     }
@@ -23,20 +22,5 @@ export class UserController {
     @Get()
     findAll() {
         return this.userService.findAll()
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.userService.findOne(+id)
-    }
-
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.userService.update(+id, updateUserDto)
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.userService.remove(+id)
     }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
+import * as argon2 from 'argon2'
 
 @Injectable()
 export class UserService {
-    create(createUserDto: CreateUserDto) {
-        return 'This action adds a new user'
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    async create(createUserDto: CreateUserDto) {
+        const existUser = await this.userRepository.findOne({
+            where: {
+                email: createUserDto.email,
+            },
+        })
+
+        if (existUser) throw new BadRequestException('This email already exist')
+
+        const user: User = await this.userRepository.save({
+            email: createUserDto.email,
+            password: await argon2.hash(createUserDto.password),
+        })
+
+        return { user }
     }
 
     findAll() {
@@ -14,13 +35,5 @@ export class UserService {
 
     findOne(id: number) {
         return `This action returns a #${id} user`
-    }
-
-    update(id: number, updateUserDto: UpdateUserDto) {
-        return `This action updates a #${id} user`
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} user`
     }
 }
